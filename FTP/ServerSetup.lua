ccd89cbf86735a53a0a87a21505b3f9b031a4806
Creator <wassil.janssen@gmail.com>
2016-03-03 13:10:38 +0200

    Working on Setup

--- FTP/ServerSetup.lua
+++ FTP/ServerSetup.lua
@@ -2,6 +2,12 @@
 	Server Setup
 ]]--
 
+local function writeTo(path, data)
+	local file = fs.open(".ftp/"...path,"w")
+	file.write(data)
+	file.close()
+end
+
 local default = [[
 list:admin
 exists:admin
@@ -22,3 +28,14 @@ getDir:admin
 local file = fs.open(".ftp/permissions","w")
 file.write(default)
 file.close()
+
+term.write("Which side is the modem on: ")
+local side = read()
+writeTo("side",side)
+
+term.write("Which is the request port?")
+local re = read()
+term.write("Which is the authentification port?")
+local au = read()
+
+writeTo("ports",re.."\n"..au)
